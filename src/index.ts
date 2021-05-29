8435510956627a273f613d0c91a72dc7528cc632
Philip Heltweg <pheltweg@gmail.com>
2021-05-29 20:41:00 +0200

    added missing exports

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 export * from "./logic/Semantic";
 export * from "./logic/Syntax";
 export * from "./logic/World";
+export * from "./logic/WorldPreference";
 
 export * from "./util/functions";
 export * from "./util/parse";
+export * from "./util/serialize";
