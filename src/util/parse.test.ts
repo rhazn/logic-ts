472c94ecc79295d20341db686b43e534324925f4
Philip Heltweg <pheltweg@gmail.com>
2021-05-31 18:13:32 +0200

    WIP binary representation of ranking function

--- src/util/parse.test.ts
+++ src/util/parse.test.ts
@@ -1,7 +1,12 @@
 import { PropositionalSyntax } from "../logic/Syntax";
 import { PropositionalWorld } from "../logic/World";
 import { WorldPreference } from "../logic/WorldPreference";
-import { propositionalWorldParser, worldPreferenceParser } from "./parse";
+import {
+    propositionalWorldParser,
+    propositionalWorldParserBinary,
+    worldPreferenceParser,
+    worldPreferenceParserBinary,
+} from "./parse";
 
 describe("parsing strings", () => {
     describe("parsing propositional worlds", () => {
@@ -16,6 +21,25 @@ describe("parsing strings", () => {
         ])("parse: %j", (input: string, expected: PropositionalWorld) => {
             expect(worldParser(input).assignment).toEqual(expected.assignment);
             expect(worldParser(input).syntax).toEqual(expected.syntax);
+        });
+    });
+
+    describe("parsing propositional worlds as binary", () => {
+        const syntax: PropositionalSyntax = new Set(["a", "b", "c"]);
+        const worldParser = propositionalWorldParserBinary(syntax);
+
+        test.each([
+            [4, new PropositionalWorld(syntax, new Set(["a"]))],
+            [6, new PropositionalWorld(syntax, new Set(["a", "b"]))],
+            [7, new PropositionalWorld(syntax, new Set(["a", "b", "c"]))],
+            [0, new PropositionalWorld(syntax, new Set([]))],
+        ])("parse: %j", (input: number, expected: PropositionalWorld) => {
+            const buffer = new ArrayBuffer(1);
+            const view = new Uint8Array(buffer);
+            view[0] = input;
+
+            expect(worldParser(buffer).assignment).toEqual(expected.assignment);
+            expect(worldParser(buffer).syntax).toEqual(expected.syntax);
         });
     });
 
@@ -48,6 +72,40 @@ describe("parsing strings", () => {
             [`[]`, new WorldPreference([])],
         ])("parse: %j", (input: string, expected: WorldPreference) => {
             expect(preferenceParser(input).data).toEqual(expected.data);
+        });
+    });
+
+    describe("parsing world preferences as binary", () => {
+        const syntax: PropositionalSyntax = new Set(["a", "b", "c"]);
+        const preferenceParser = worldPreferenceParserBinary(syntax);
+
+        test.each([
+            [
+                new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 4, 2]).buffer,
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                ]),
+            ],
+            [
+                new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 4, 2, 0, 0, 0, 1, 0, 0, 0, 0, 5]).buffer,
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                    [new PropositionalWorld(syntax, new Set(["a", "c"]))],
+                ]),
+            ],
+            [
+                new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 1, 4, 2, 0, 0, 0, 1, 0, 0, 0, 0, 5]).buffer,
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                    [],
+                    [new PropositionalWorld(syntax, new Set(["a", "c"]))],
+                ]),
+            ],
+            [new Uint8Array([]).buffer, new WorldPreference([])],
+        ])("parse: %o", (input: ArrayBuffer, expected: WorldPreference) => {
+            const inputPreference = preferenceParser(input);
+
+            expect(expected.data).toEqual(inputPreference.data);
         });
     });
 });
--- src/util/serialize.test.ts
+++ src/util/serialize.test.ts
@@ -1,7 +1,12 @@
 import { PropositionalSyntax } from "../logic/Syntax";
 import { PropositionalWorld } from "../logic/World";
 import { WorldPreference } from "../logic/WorldPreference";
-import { propositionalWorldSerializer, worldPreferenceSerializer } from "./serialize";
+import {
+    propositionalWorldBinarySerializer,
+    propositionalWorldSerializer,
+    worldPreferenceBinarySerializer,
+    worldPreferenceSerializer,
+} from "./serialize";
 
 describe("serialize entities", () => {
     describe("serialize propositional worlds", () => {
@@ -14,6 +19,21 @@ describe("serialize entities", () => {
             [new PropositionalWorld(syntax, new Set([])), `[]`],
         ])("serialize: %o", (input: PropositionalWorld, expected: string) => {
             expect(propositionalWorldSerializer(input)).toEqual(expected);
+        });
+    });
+
+    describe("serialize propositional worlds as binary", () => {
+        const syntax: PropositionalSyntax = new Set(["a", "b", "c"]);
+
+        test.each([
+            [new PropositionalWorld(syntax, new Set(["a"])), 4],
+            [new PropositionalWorld(syntax, new Set(["a", "b"])), 6],
+            [new PropositionalWorld(syntax, new Set(["a", "b", "c"])), 7],
+            [new PropositionalWorld(syntax, new Set([])), 0],
+        ])("serialize: %o", (input: PropositionalWorld, expected: number) => {
+            const serializedView = new Int8Array(propositionalWorldBinarySerializer(input));
+
+            expect(serializedView[0]).toEqual(expected);
         });
     });
 
@@ -45,6 +65,47 @@ describe("serialize entities", () => {
             [new WorldPreference([]), `[]`],
         ])("parse: %o", (input: WorldPreference, expected: string) => {
             expect(worldPreferenceSerializer(input)).toEqual(expected);
+        });
+    });
+
+    describe("serialize world preferences as binary", () => {
+        const syntax: PropositionalSyntax = new Set(["a", "b", "c"]);
+
+        test.each([
+            [
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                ]),
+                new DataView(new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 4, 2]).buffer),
+            ],
+            [
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                    [new PropositionalWorld(syntax, new Set(["a", "c"]))],
+                ]),
+                new DataView(
+                    new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 0, 4, 2, 0, 0, 0, 1, 0, 0, 0, 0, 5]).buffer,
+                ),
+            ],
+            [
+                new WorldPreference([
+                    [new PropositionalWorld(syntax, new Set(["a"])), new PropositionalWorld(syntax, new Set(["b"]))],
+                    [],
+                    [new PropositionalWorld(syntax, new Set(["a", "c"]))],
+                ]),
+                new DataView(
+                    new Uint8Array([1, 0, 0, 0, 3, 0, 0, 0, 2, 0, 0, 0, 1, 4, 2, 0, 0, 0, 1, 0, 0, 0, 0, 5]).buffer,
+                ),
+            ],
+            [new WorldPreference([]), new DataView(new Uint8Array([]).buffer)],
+        ])("parse: %o", (input: WorldPreference, expected: DataView) => {
+            const serialized = new DataView(worldPreferenceBinarySerializer(input));
+
+            expect(expected.byteLength).toEqual(serialized.byteLength);
+
+            for (let i = 0; i < expected.byteLength; i++) {
+                expect(expected.getUint8(i)).toEqual(serialized.getUint8(i));
+            }
         });
     });
 });
